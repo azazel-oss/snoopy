1e90d355d1035148ce186496c68a9a49f471b572
Asad Mahmood <asadmhmd1234@gmail.com>
2024-07-21 21:23:11 +0530

    improve landing home page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <>home page</>
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import { inter } from '../ui/fonts'
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center md:h-screen gap-40">
+      <div
+        className={`${inter.className} flex flex-col gap-32 font-black text-9xl text-center`}
+      >
+        <section>
+          Let&apos;s get <span className="text-red-600">snoopin&apos;</span>
+        </section>
+        <section className="flex gap-40 text-xl justify-center">
+          <Link href={'/dashboard'}>
+            <Button className="inline px-7 p-7">Dashboard</Button>
+          </Link>
+          <Link href={'/login'}>
+            <Button className="inline px-7 p-7">Login</Button>
+          </Link>
+        </section>
+      </div>
+    </main>
+  )
 }
