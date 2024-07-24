3277bd90faafa383562009760586300913640756
Asad Mahmood <asadmhmd1234@gmail.com>
2024-07-24 17:09:20 +0530

    change log out icon

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import NavLinks from '@/app/ui/nav-links'
-import { PowerIcon } from '@heroicons/react/24/outline'
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/outline'
 import { signOut } from 'next-auth/react'
 
 export default function SideNav() {
@@ -30,7 +30,7 @@ export default function SideNav() {
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-logo-bg-light p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
+            <ArrowRightStartOnRectangleIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
         </form>
