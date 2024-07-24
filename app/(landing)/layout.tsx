6106e85991eafb6b72e185ee2c50518ad875cc0a
Asad Mahmood <asadmhmd1234@gmail.com>
2024-07-24 17:03:56 +0530

    make home layout logo clickable

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function RootLayout({
   children,
@@ -11,13 +12,16 @@ export default function RootLayout({
       <body className="flex w-full">
         <section className="flex justify-center items-center w-1/2 bg-logo-bg-light">
           <div>
-            <Image
-              src={'/snoopy-logo.png'}
-              width={700}
-              height={700}
-              alt="Snoopy logo image"
-              priority
-            />
+            <Link href={'/'}>
+              <Image
+                src={'/snoopy-logo.png'}
+                width={700}
+                height={700}
+                alt="Snoopy logo image"
+                className="transition ease-in-out hover:scale-110 border-app-red-secondary rounded-xl hover:border-4"
+                priority
+              />
+            </Link>
           </div>
         </section>
         <section className="w-1/2">{children}</section>
